4cce944da58822b7aca8f039e2379fcab303be79
hamakana0422 <icaam0422@gmail.com>
2022-05-08 14:35:51 +0900

    ルーティングの修正とUserContorollerの追加

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //先生
+    public function t_login()
+    {
+        return view('teather.login');
+    }
+
+    public function t_create()
+    {
+        return view('teacher.create');
+    }
+
+    public function registerstudent()
+    {
+        return view('teacher.registerforstudent');
+    }
+
+    public function t_edit()
+    {
+        return view('teacher.edit');
+    }
+
+    //生徒
+    public function s_login()
+    {
+        return view('student.login');
+    }
+
+    public function f_login()
+    {
+        return view('student.firstlogin');
+    }
+
+    public function s_edit()
+    {
+        return view('student.edit');
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -24,21 +24,21 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 // 以下、先生用
-Route::get('/teacher/login', [App\Http\Controllers\MemberController::class, 't_login']);
+Route::get('/teacher/login', [App\Http\Controllers\UserController::class, 't_login']);
 
 // Route::get('/teacher/login', [App\Http\Controllers\HomeController::class, 'index']);
 
-Route::get('/teacher/create', [App\Http\Controllers\MemberController::class, 't_login']);
+Route::get('/teacher/create', [App\Http\Controllers\UserController::class, 't_create']);
 
-Route::get('/teacher/registerforstudent', [App\Http\Controllers\MemberController::class, 'registerstudent']);
+Route::get('/teacher/registerforstudent', [App\Http\Controllers\UserController::class, 'registerstudent']);
 
-Route::get('/teacher/edit', [App\Http\Controllers\MemberController::class, 't_edit']);
+Route::get('/teacher/edit', [App\Http\Controllers\UserController::class, 't_edit']);
 
 // 以下、生徒用
-Route::get('/student/login', [App\Http\Controllers\MemberController::class, 's_login']);
+Route::get('/student/login', [App\Http\Controllers\UserController::class, 's_login']);
 
 //Route::post('/student/login',[App\Http\Controllers\Auth\LoginController::class, 'login']);
 
-Route::get('/student/firstlogin', [App\Http\Controllers\MemberController::class, 'f_login']);
+Route::get('/student/firstlogin', [App\Http\Controllers\UserController::class, 'f_login']);
 
-Route::get('/student/edit', [App\Http\Controllers\MemberController::class, 's_edit']);
+Route::get('/student/edit', [App\Http\Controllers\UserController::class, 's_edit']);
