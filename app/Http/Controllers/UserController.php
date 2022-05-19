9c42af436f1ead18beb41f48a4e9e5cc0653e3cb
yu0619 <95677440+yu0619@users.noreply.github.com>
2022-05-19 22:11:06 +0900

    Update UserController.php
    先生のrequestの名前変更

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -44,8 +44,8 @@ class UserController extends Controller
         //【todo】Validation追加
         User::create([
             'user_type' => 0,
-            'first_name' => $request->name,
-            'last_name'=> $request->name,
+            'first_name' => $request->first_name,
+            'last_name'=> $request->last_name,
             'email'=> $request->email,
             'password'=> Hash::make($request->password)
 
