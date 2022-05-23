2e950653f18d9fd4baf0074f106a41918e88a0da
yu0619 <yuu000610@gmail.com>
2022-05-23 18:17:37 +0900

    レポートコントローラー修正

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -14,7 +14,7 @@ class ReportController extends Controller
     public function t_report($id)
     {
         $user = User::find($id);
-        $reports = Report::all();
+        $reports = Report::where('user_id', $user->id)->orderBy('created_at', 'desc')->get();
         //dd($user);
         return view('teacher.report',[
             'user' => $user,
