fe3b1267e8882aff7dbdceb2af9dbbeb07b01de4
yu0619 <yuu000610@gmail.com>
2022-06-05 18:57:31 +0900

    体調管理編集・削除機能実装

--- resources/views/student/reportdelete.blade.php
+++ resources/views/student/reportdelete.blade.php
@@ -9,9 +9,8 @@
             <div class="card">
             <div class="card-header">{{ __('東京小学校　体調管理システム') }}</div>
                 <div class="card-body">
-                    <form method="POST" action="{{ url('/student/login') }}">
-                        @csrf
-                        <a href=""><button type="button">前画面へ戻る</button></a>
+
+                        <a href="/student/manage"><button type="button">前画面へ戻る</button></a>
                         <div style="border: #808000 solid 1px; font-size: 100%; padding: 3px; background-color:#87CEFA;">過去に報告した体罰について、編集や削除が出来ます</div>
                         <div>
                             <table class="table table-striped table-bordered">
@@ -26,101 +25,23 @@
                                 <th></th>
                                 </tr>
                             </thead>
-                            <tbody>
-                                <tr>
-                                    <td>2022/5/1</td>
-                                    <td>快調</td>
-                                    <td>36.5</td>
-                                    <td>3</td>
-                                    <td>8</td>
-                                    <td><button>編集する</button></td>
-                                    <td><button>削除する</button></td>
-                                </tr>
-                                <tr>
-                                    <td>2022/5/2</td>
-                                    <td>普通</td>
-                                    <td>36.5</td>
-                                    <td>3</td>
-                                    <td>8</td>
-                                    <td><button>編集する</button></td>
-                                    <td><button>削除する</button></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td><button>編集する</button></td>
-                                    <td><button>削除する</button></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td><button>編集する</button></td>
-                                    <td><button>削除する</button></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td><button>編集する</button></td>
-                                    <td><button>削除する</button></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td><button>編集する</button></td>
-                                    <td><button>削除する</button></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td><button>編集する</button></td>
-                                    <td><button>削除する</button></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td><button>編集する</button></td>
-                                    <td><button>削除する</button></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td><button>編集する</button></td>
-                                    <td><button>削除する</button></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td><button>編集する</button></td>
-                                    <td><button>削除する</button></td>
-                                </tr>
-                            </tbody>
-                            </table>
+                            @foreach ($reports as $report)
+                            <tr>
+                                <td>{{ $report->created_at }}</td>
+                                <td>{{ $report->condition }}</td>
+                                <td>{{ $report->temperature }}</td>
+                                <td>{{ $report->meal }}</td>
+                                <td>{{ $report->sleep }}</td>
+                                <td><a href="{{ url('/student/report/'.$report->id)}}"><button type="submit" class="btn btn-primary">
+                                    {{ __('編集する') }}
+                                </td></button>
+                                <form action="{{ url('/report/delete/' . $report->id )}}" method="POST">
+                                @csrf
+                                <td><input type="submit" class="btn btn-danger btn-dell" value="削除"></td>
+                                </form>
+                            </tr>
+                            @endforeach
                         </div>
-                    
                 </div>
             </div>
         </div>
