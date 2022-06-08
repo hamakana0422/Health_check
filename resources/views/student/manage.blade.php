889e6fec47906ff3538389a83832ca7519a806a7
yu0619 <yuu000610@gmail.com>
2022-06-08 17:02:42 +0900

    体調報告画面　プルダウン方式に変更

--- resources/views/student/manage.blade.php
+++ resources/views/student/manage.blade.php
@@ -17,25 +17,30 @@
                         <div>
                             <label for="day">報告日</label></br>
                             <label><input type="date" name="day" required autofocus></label>
-                            <!-- <input type="text" name="year" size = "6">年 -->
-                            <!-- <input type="text" name="moon" size = "3">月 -->
-                            <!-- <input type="text" name="day" size = "3">日 -->
                         </div>
                         <div>
                             <label for="condition">今朝の体調はどうですか？</label></br>
-                            <input type="text" name="condition" size="30">
+                            <!-- <input type="text" name="condition" size="30" required> -->
+                            <select name="condition" id="condition">
+                                <option value="快調">快調</option>
+                                <option value="普通">普通</option>
+                                <option value="少し悪い">少し悪い</option>
+                                <option value="不調">不調</option>
+                                <option value="風邪">風邪</option>
+                            </select>
+
                         </div>
                         <div>
                             <label for="temperature">今朝の体温は何度ですか？</label></br>
-                            <input type="text" name="temperature" size="30">
+                            <input type="number" name="temperature" value="36.0" min="34.0" max="42.0" step = "0.1" required>
                         </div>
                         <div>
                             <label for="sleep">昨日の夜は何時間寝ましたか？</label></br>
-                            <input type="text" name="sleep" size="30">
+                            <input type="number" name="sleep" value="7.0" min="0" max="24.0" step = "0.5" required>
                         </div>
                         <div>
                             <label for="meal">昨日は何回食事をとりましたか？</label></br>
-                            <input type="text" name="meal" size="30">
+                            <input type="number" name="meal" value="3" min="0" max="10" step = "1" required>
                         </div>
                     </form>
                     <button type="submit" class="btn btn-primary my-3">報告する</button>
